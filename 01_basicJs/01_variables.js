854cb8c126f8dd5d54a49fbc600d50b431a6d79f
Mohd Kasif Khan <developer786kasif@gmail.com>
2024-03-12 08:51:19 +0000

    Let, const and var ki kahani

---
+++ 01_basicJs/01_variables.js
@@ -0,0 +1,15 @@
+const studentId = 54321;
+let   studentId1 = 12345;
+var student_name = "kaisf khan";
+studentCity = "Lucknow";
+// Change variables
+ //studentId = 98765;  not change beacuse its constant values
+  studentId1 = 678910;
+ student_name = "Zohaan khan";
+ studentCity = "Mumbai";
+
+//  Prefer to not use to var
+//  beacuse of issue to block scope and functional scope in java
+
+
+console.table([studentId,studentId1,student_name,studentCity]);
